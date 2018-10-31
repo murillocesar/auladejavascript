84de8115f7d23a0173f784a7e357e369a6883c2b
murillocesar <murillocbispo@gmail.com>
2018-10-30 21:58:58 -0300

    modificação do css,html e javascrtipt

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+let nomeDoUsuario = 'Murillo Cesar';
+let idade = 18;
+let vivo = false;
+
+let saida1 ='Olá,' + '! Tudo bem com você';
+saida1 = `Olá, ${nomeDoUsuario}! Você tem ${idade} anos`
+nomeDoUsuario = prompt(`Qual seu nome?`);
+idade = prompt(`Quantos anos você tem?`);
+vivo = prompt(`Você esta vivo?`);
+alert(`${saida1}`);
+
+
+
+
+console.log ('Hello world');
+// alert('Hello world');
+// console.log('Passou do alert');
+
